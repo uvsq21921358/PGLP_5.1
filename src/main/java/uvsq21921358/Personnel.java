c8fc009f00d1ff9d808e3cfc30a03aed0da7c793
SIMPORE <nayimasimpore@gmail.com>
2020-04-08 15:37:15 +0200

    Ajout des classes de base personnel

---
+++ src/main/java/uvsq21921358/Personnel.java
@@ -0,0 +1,10 @@
+package uvsq21921358;
+
+import java.io.Serializable;
+
+public abstract class Personnel implements Serializable {
+
+	//Classe de base 
+	private static final long serialVersionUID = 1L;
+	abstract public boolean isGroupe();
+}
