da8c668f3ec5b8a8c212522857a3b5da04e0bc4f
SIMPORE <nayimasimpore@gmail.com>
2020-04-08 15:57:28 +0200

    Serialisation des classes

---
+++ src/main/java/uvsq21921358/PersonnelGroupeSerialDAO.java
@@ -0,0 +1,16 @@
+package uvsq21921358;
+
+public class PersonnelGroupeSerialDAO extends SerialDAO<PersonnelGroupe> {
+
+	//
+	public String getFilename(String id) {
+		return id + ".pg";
+	}
+
+	
+	public String getFilename(PersonnelGroupe objet) {
+		return getFilename(objet.getId());
+	}
+}
+
+
